73c1f96b3b48b7f1182ef658863dbd146650404c
rimartt <rimartencinares@thelewiscollege.edu.ph>
2024-06-08 12:46:10 +0800

    second

--- index.php
+++ index.php
@@ -147,3 +147,15 @@
     </form>
 </body>
 </html>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>fdfdsf</h1>
+</body>
+</html>
